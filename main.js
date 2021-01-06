5439d199be9e7361b7253d72d500046cbf526415
Andrey Khodenkov <khodenkov2807@gmail.com>
2021-01-06 17:53:13 +0300

    Todo List

---
+++ main.js
@@ -0,0 +1,42 @@
+var vue = new Vue({
+    el: '#vue',
+    data: {
+        title: 'Title',
+        counter: 0,
+        input: '',
+        name: [],
+        date: 1
+    },
+    methods: {
+        add: function (post) {
+            if (post) {
+                this.name.push({
+                    title:post,
+                    dt:new Date()
+                })
+                this.input = ''
+                console.log(this.name);
+            }
+        },
+        remove:function(ind,event){
+            console.log(event);
+           return this.name.splice(ind,1)
+        },
+        toUpper: function(item){
+           return item.toUpperCase()
+        }
+    },
+    computed: {
+        count:function(){
+            return this.name.length * 2
+        }
+    },
+    watch: {
+        input(value){
+            if(value.length > 15){
+                this.input = ''
+            }
+        }
+    }
+    
+})
